fd46994cbd021624d54531251d720a332d2f1b68
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-05-04 22:56:54 +0300

    Added Screen; changed README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # suoi_rwin
-SUOI's Rendering and WINdow management solution (RWIN)
+SUOI's Rendering and Window INitialization solution (RWIN)
---
+++ src/screen/mod.rs
@@ -0,0 +1,23 @@
+pub struct Screen {
+    width: u32,
+    height: u32,
+}
+
+impl Screen {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
+    }
+
+    pub fn aspect_ratio(&self) -> f32 {
+        self.width as f32 / self.height as f32
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        unsafe {
+            gl::Viewport(0, 0, width as i32, height as i32);
+        }
+        
+        self.width = width;
+        self.height = height;
+    }
+}

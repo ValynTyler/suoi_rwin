6caf9284ce7434971c52e773e305adbf83263c6c
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-05-05 00:36:11 +0300

    Update Mouse

--- src/mouse/mod.rs
+++ src/mouse/mod.rs
@@ -11,10 +11,16 @@ pub struct Mouse {
     right_button: MouseButton,
     
     position: Vector2,
+    last_pos: Vector2,
 }
 
 impl Mouse {
+    pub fn delta(&self) -> Vector2 {
+        self.position - self.last_pos
+    }
+
     pub fn update_position(&mut self, pos: Vector2) {
+        self.last_pos = self.position;
         self.position = pos;
     }
 }

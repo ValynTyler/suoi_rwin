85b30ad6dab5e49a92d80de3a63a34d13f23ebab
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-05-05 15:56:51 +0300

    Update Camera

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,9 +1,47 @@
+use nerd::matrix::Matrix4;
+
+use crate::Screen;
+
 pub enum ProjectionType {
     Ortho,
     Perspective,
 }
 
-#[allow(unused)]
+pub struct ClippingPlanes {
+    near: f32,
+    far: f32,
+}
+
+impl ClippingPlanes {
+    pub fn new(near: f32, far: f32) -> Self {
+        Self { near, far }
+    }
+}
+
 pub struct Camera {
     projection_type: ProjectionType,
+    clipping_planes: ClippingPlanes,
+    fov: f32,
+}
+
+impl Camera {
+    pub fn new(projection: ProjectionType, clipping: ClippingPlanes, fov: f32) -> Self {
+        Self {
+            projection_type: projection,
+            clipping_planes: clipping,
+            fov,
+        }
+    }
+
+    pub fn projection_matrix(&self, screen: &Screen) -> Matrix4 {
+        match self.projection_type {
+            ProjectionType::Perspective => nerd::matrix::Matrix4::perspective(
+                self.fov,
+                screen.aspect_ratio(),
+                self.clipping_planes.near,
+                self.clipping_planes.far,
+            ),
+            ProjectionType::Ortho => todo!(),
+        }
+    }
 }

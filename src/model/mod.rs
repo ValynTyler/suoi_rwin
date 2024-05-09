adb296cecf0a21f5fde08070ff1dc409071041ea
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-05-09 09:16:07 +0300

    Added model_matrix to Model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,6 +1,7 @@
 pub mod vertex;
 
 use suoi_simp::Obj;
+use suoi_types::{Matrix4, Transform};
 pub use vertex::*;
 
 pub mod mesh;
@@ -13,11 +14,12 @@ pub const SIZE_OF_FLOAT: GLsizei = std::mem::size_of::<GLfloat>() as GLsizei;
 #[allow(unused)]
 pub struct Model {
     meshes: Vec<Mesh>,
+    transform: Transform,
 }
 
 impl From<Obj> for Model {
     fn from(value: Obj) -> Self {
-        let mut model = Self { meshes: vec![] };
+        let mut model = Self { meshes: vec![], transform: Transform::default() };
         
         for mesh in value.meshes() {
             model.meshes.push(mesh.into());
@@ -32,5 +34,9 @@ impl Model {
         for mesh in &self.meshes {
             mesh.draw()
         }
+    }
+
+    pub fn model_matrix(&self) -> Matrix4 {
+        Matrix4::translate(self.transform.position())
     }
 }

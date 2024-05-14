3173c659f94ea2ffd15ec5ee4f8bbce4c36c031e
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-05-14 06:52:29 +0300

    Wrote test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use suoi_types::{Matrix, Matrix4, Quaternion, Rad, Transform, Vector, Vector3};
 
+fn main() {
+    let q = Quaternion::axis_angle(Vector3::new(0.3, 0.2, -0.5).unit(), Rad(1.234));
+
+    let mut t = Transform::default();
+    t.set_position(Vector3::new(0.3, 1.234, 96.240));
+    // t.set_rotation(q);
+
+    let u = t.up();
+    let r = t.right();
+    let f = t.forward();
+
+    let t = Vector3 {
+        x: -r.dot(t.position()),
+        y: -u.dot(t.position()),
+        z: -f.dot(t.position()),
+    };
+
+    let mat = Matrix4::look_at(f, u, r, t).transpose();
+
+    println!("{}", &mat * &mat.inverse());
 }
